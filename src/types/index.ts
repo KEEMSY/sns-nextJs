00adcd232c1ffd0cad19588e03ec5f49536d57c9
keemsy <phzzz@naver.com>
2024-11-10 19:31:57 +0900

    Add: 사용하는 타입을 관리하는 index.ts 추가 -  존재하는 다양한 타입에 대한 추출 시, 단일 경로로 사용하기 위해 index.ts 으로 작성

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface User {
+  userId: string;
+  name: string;
+  username: string;
+  color: string;
+  avatar: string;
+}
+
+export interface Club {
+  id: string;
+  name: string;
+  color: string;
+  memberCount: number;
+} 
